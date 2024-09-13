ceb09c68bae92d27854ea7bbb229ed9040d17a2d
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-09-13 16:43:40 +0200

    Added first six soulutions

---
+++ part_3_Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+sum = a + b
+subtracion = a - b
+
+if a != 0 and b != 0:
+    product = a * b
+    
+print(sum)
+print(subtracion)
+print(product)
---
+++ part_4_Python_Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+intiger_division = a // b
+float_division = a / b
+
+print(intiger_division)
+print(float_division)
