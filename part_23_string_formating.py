0471c53c80b4d27b0aeebba5531218e8d26885a4
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-11 20:17:13 +0200

    added twenty three soultion

--- part_23_string_formating.py
+++ part_23_string_formating.py
@@ -1,9 +1,8 @@
-def decimal(x):
-    for i in range(int(x)):
-        print(i) 
+def print_formatted(number):
+    width = len(bin(number)) - 2
 
-def octal(x):
-    
+    for i in range(1, number + 1):
+        print(f"{i:>{width}} {oct(i)[2:]:>{width}} {hex(i)[2:].upper():>{width}} {bin(i)[2:]:>{width}}")
 
-x = input()
-decimal(x)
+number = int(input())
+print_formatted(number)
