49a8d814ccadcfad1ba1303a418e408b4d4619b8
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-14 21:25:16 +0200

    added twenty five solution

---
+++ part_25_capitalize.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    s = s.split(' ')
+    for i in range(len(s)):
+        s[i] = s[i].capitalize()
+    return ' '.join(s)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+    print(result)
