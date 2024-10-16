a67b7da40af2f726c866088f82acaf3d1b10dd17
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-16 12:42:33 +0200

    added second part in progress twenty six minion game

--- part_26_minion_game.py
+++ part_26_minion_game.py
@@ -1,10 +1,22 @@
 def minion_game(string):
     arr = []
+    count_number = 0
     for i in string:
+        x = string.index(i)
+        string = string[(x):]
         if i.lower() in "aeiou":
-            index = int(string.index(i))
-            arr.append(string[index:])    
-    print(arr)       
+            for i in range(len(string)):
+                if i != 0:
+                    arr.append(string[:-i])
+                else:
+                    arr.append(string)
+    for items in arr:
+        x = arr.count(items)
+        count_number += x
+        
+
+        
+    print(arr)
     
         
 
