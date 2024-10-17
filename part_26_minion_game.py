ae7dd0ca1785fad6c867448d106a17363cd05711
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-17 10:28:36 +0200

    added solution of twenty six

--- part_26_minion_game.py
+++ part_26_minion_game.py
@@ -1,24 +1,21 @@
 def minion_game(string):
-    arr = []
-    count_number = 0
-    for i in string:
-        x = string.index(i)
-        string = string[(x):]
-        if i.lower() in "aeiou":
-            for i in range(len(string)):
-                if i != 0:
-                    arr.append(string[:-i])
-                else:
-                    arr.append(string)
-    for items in arr:
-        x = arr.count(items)
-        count_number += x
-        
+    vowels = 'AEIOU'
+    kevin_score = 0
+    stuart_score = 0
+    length = len(string)
 
-        
-    print(arr)
-    
-        
+    for i in range(length):
+        if string[i] in vowels:
+            kevin_score += (length - i)
+        else:
+            stuart_score += (length - i)
+
+    if kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    elif stuart_score > kevin_score:
+        print(f"Stuart {stuart_score}")
+    else:
+        print("Draw")
 
 if __name__ == '__main__':
     s = input()
