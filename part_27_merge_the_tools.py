2d0e5cc07bad0dd0da148e681cfdca399e952415
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-18 16:08:22 +0200

    added merge the tools in process

---
+++ part_27_merge_the_tools.py
@@ -0,0 +1,10 @@
+def merge_the_tools(s, k):
+    t1 = [s[i:i+k] for i in range(0, len(s), k)]
+    u1 = []
+    num = 0
+    
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
