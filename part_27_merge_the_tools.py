e573f017f429792633ebc49665e3807dc273a720
Tomasz Wesołek <tomek.wesolek1@gmail.com>
2024-10-28 11:25:27 +0100

    added twenty seven solution

--- part_27_merge_the_tools.py
+++ part_27_merge_the_tools.py
@@ -1,10 +1,11 @@
 def merge_the_tools(s, k):
-    t1 = [s[i:i+k] for i in range(0, len(s), k)]
-    u1 = []
-    num = 0
+    for i in range(0, len(string), k):
+        uniq = ''
+        for c in string[i : i+k]:
+            if (c not in uniq):
+                uniq+=c
+        print(uniq)
     
-
-
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
